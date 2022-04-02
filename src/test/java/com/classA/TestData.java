78b662afc69898a1fcf7eb3dd4eb2057344f89cc
Dhivyalakshmi16 <dhivyalakshmi312@gmail.com>
2022-04-02 20:00:03 +0530

    class3

--- src/test/java/com/classA/TestData.java
+++ src/test/java/com/classA/TestData.java
@@ -1,5 +1,20 @@
 package com.classA;
 
+
+
 public class TestData {
+
+	
+public void testname() {
+	System.out.println("greens");
+}
+public void testnumber() {
+	System.out.println("23");
+
+}
+public void testId() {
+	System.out.println("123456789");
+
+}
 
 }
